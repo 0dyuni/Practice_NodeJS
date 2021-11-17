40f12af99bb9efc6272decb63463e0c4cc9688f2
0dyuni <tjdudwns13@naver.com>
2021-11-17 19:02:31 +0900

    Encrypting passwords using bcrypt

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 //express 모듈 가져오기
-const express = require('express');
+const express = require("express");
 //새로운 express 만들기
 const app = express();
 //포트 설정
@@ -7,7 +7,7 @@ const port = 3000;
 //const bodyParser = require('body-parser');
 const { User } = require("./models/User");
 //dev,prod 환경 구분을 위해서 만든 key를 가져옴
-const config = require('./config/key');
+const config = require("./config/key");
 
 //application/x-www-form-urlencoded 이런 데이터는 분석해서 가져온다.
 //app.use(bodyParser.urlencoded({extended: true}));
@@ -17,26 +17,25 @@ app.use(express.json());
 //app.use(bodyParser.json());
 
 //몽구스 연결, 에러표시,연결표시
-const mongoose = require('mongoose');
-mongoose.connect(config.mongoURI,)
-    .then(() => console.log('MongoDB Connected'))
-    .catch(err => console.log(err))
+const mongoose = require("mongoose");
+mongoose
+  .connect(config.mongoURI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
 
+app.get("/", (req, res) => res.send("Hello World 쿠쿠루삥뽕"));
 
-
-app.get('/', (req, res) => res.send('Hello World 쿠쿠루삥뽕'));
-
-app.post('/register', (req, res) => {
-    //회원가입 할 때 입력한 정보들을 client에서 가져오면
-    //그것들을 데이터 베이스에 넣어준다.
-    const user = new User(req.body)
-
-    user.save((err, userInfo) => {
-        if (err) return res.json({ success: false, err })
-        return res.status(200).json({
-            success: true
-        })
-    })
-})
+// register 라우터
+app.post("/register", (req, res) => {
+  //회원가입 할 때 입력한 정보들을 client에서 가져오면
+  //그것들을 데이터 베이스에 넣어준다.
+  const user = new User(req.body);
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- models/User.js
+++ models/User.js
@@ -1,41 +1,63 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const userSchema = mongoose.Schema({
-    name: {
-        type: String,
-        maxlength: 50
-    },
-    email: {
-        type: String,
-        //빈칸 x
-        trim: true,
-        //중복x
-        unique: 1
-    },
-    password: {
-        type: String,
-        minlength: 5
-    },
-    lastname: {
-        type: String,
-        maxlength: 50
-    },
-    //계급 (관리자)
-    role: {
-        type: Number,
-        default: 0
-    },
-    image: String,
-    //유효성
-    token: {
-        type: String
-    },
-    //토큰기간
-    tokeExp: {
-        type: Number
-    }
-})
+  name: {
+    type: String,
+    maxlength: 50,
+  },
+  email: {
+    type: String,
+    //빈칸 x
+    trim: true,
+    //중복x
+    unique: 1,
+  },
+  password: {
+    type: String,
+    minlength: 5,
+  },
+  lastname: {
+    type: String,
+    maxlength: 50,
+  },
+  //계급 (관리자)
+  role: {
+    type: Number,
+    default: 0,
+  },
+  image: String,
+  //유효성
+  token: {
+    type: String,
+  },
+  //토큰기간
+  tokeExp: {
+    type: Number,
+  },
+});
 
-const User = mongoose.model('User', userSchema)
+userSchema.pre("save", function (next) {
+  var user = this;
 
-module.exports = {User}
+  // pssword가 변환 될 때만 bcrypt을 이용해서 암호화 해준다
+  //라는 조건문 
+  if (user.isModified("password")) {
+    
+    // 비밀번호를 암호화 시킨다. (salt를 이용)
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+      if (err) return next(err);
+
+      bcrypt.hash(user.password, salt, function (err, hash) {
+        if (err) return next(err);
+        user.password = hash;
+        next();
+      });
+    });
+  }
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = { User };
