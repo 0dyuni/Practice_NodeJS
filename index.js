c4cdf19fdc0a650e3a0d06f3ea7e656f63a7680d
0dyuni <tjdudwns13@naver.com>
2021-11-18 14:26:01 +0900

    logout

--- index.js
+++ index.js
@@ -91,4 +91,18 @@ app.get("/api/users/auth", auth, (req, res) => {
   });
 });
 
+// logout 라우터
+//데이터베이스에서 token을 지운다.
+// 데이터베이스에서 token을 지우면 auth로 클라이언트와 토큰이 맞지 않으므로 로그인이 풀리는것을 이용한다.
+app.get("/api/users/logout", auth, (req, res) => {
+  // 로그아웃 하려는 user를 데이터베이스에서 찾아서
+  //업데이트 시켜준다.
+  User.findOneAndUpdate({ _id: req.user._id }, { token: "" }, (err, user) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).send({
+      success: true,
+    });
+  });
+});
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
